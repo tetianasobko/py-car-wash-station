05cf51d324574e2fc0635229a016fd1de14c3e83
Tetiana Sobko <sobkotiana@gmail.com>
2024-08-24 18:14:59 +0300

    Fix method wash_single_car

--- app/main.py
+++ app/main.py
@@ -20,19 +20,18 @@ class CarWashStation:
         return total_income
 
     def serve_single_car(self, car: Car) -> float:
-        if car.clean_mark < self.clean_power:
-            price = self.calculate_washing_price(car)
-            self.wash_single_car(car)
-            return price
-
-        return 0
+        price = self.calculate_washing_price(car)
+        self.wash_single_car(car)
+        return price
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(car.comfort_class * (self.clean_power - car.clean_mark)
-                     * self.average_rating / self.distance_from_city_center, 1)
+                     * self.average_rating / self.distance_from_city_center
+                     , 1) if car.clean_mark < self.clean_power else 0
 
     def wash_single_car(self, car: Car) -> None:
-        car.clean_mark = self.clean_power
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, rate: float) -> None:
         self.average_rating = round(
